a20232b602f08818033f0ee82bda6e7271ebb547
SathvigaaBharathi <sathvigaabharathi@gmail.com>
2026-03-30 18:30:18 +0530

    Update DB config for Railway

--- app.py
+++ app.py
@@ -11,10 +11,11 @@ from functools import wraps
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'fallback_secret')
 
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', '')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'studyvault')
+app.config['MYSQL_HOST'] = os.getenv('MYSQLHOST', os.getenv('MYSQL_HOST', 'localhost'))
+app.config['MYSQL_USER'] = os.getenv('MYSQLUSER', os.getenv('MYSQL_USER', 'root'))
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQLPASSWORD', os.getenv('MYSQL_PASSWORD', ''))
+app.config['MYSQL_DB'] = os.getenv('MYSQLDATABASE', os.getenv('MYSQL_DB', 'studyvault'))
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQLPORT', os.getenv('MYSQL_PORT', 3306)))
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
